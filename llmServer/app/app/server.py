fd5c603942f3da84a9fa0bbbfe50521d5f295831
donghakl <1599zzang@gmail.com>
2024-12-06 20:33:31 +0900

    feat: langserve 정상 실행 가능

--- llmServer/app/app/server.py
+++ llmServer/app/app/server.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from langserve import add_routes
-from chain import chain_with_history
-#  langsmith
+from chain import full_chain
 from langchain_teddynote import logging
+
 
 # 프로젝트 이름을 입력합니다.
 logging.langsmith("DATASUMMERIZING")
@@ -13,9 +14,19 @@ app = FastAPI(
     description="Spin up a simple api server using Langchain's Runnable interfaces",
 )
 
+
+# 우리 서비스만 할 수 있또록 CORS 설정 필요함!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인을 허용하거나 특정 도메인을 설정
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
 add_routes(
     app,
-    chain_with_history,
+    # chain_with_history,
+    full_chain,
     path="/llm",
 )
 
