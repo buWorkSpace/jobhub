e36f69fece0cb22fbb9d8247a93d62be17364a57
farmer0010 <89842760+farmer0010@users.noreply.github.com>
2024-12-08 23:14:07 +0900

    Update SocialKakao.js

--- front/src/SocialKakao.js
+++ front/src/SocialKakao.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 const SocialKakao = () => {
-  const KAKAO_REST_API_KEY = 'ec96ef10095fbc88dedef9f54e2ddc78';  // REST API 키
-  const KAKAO_REDIRECT_URI = 'http://localhost:3000/auth';  // 리디렉션 URI
 
   // 카카오 인증 URL 생성
   const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${encodeURIComponent(KAKAO_REDIRECT_URI)}`;
